5fb5eec17a2cc5e17bcc3c6bbdfb209f9244398c
AracelySC <84749072+AracelySC@users.noreply.github.com>
2021-09-25 19:39:14 -0600

    Interfaz

---
+++ AutenticacionCoordenadas/AutenticacionCoordenadas/Controllers/UsuarioController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutenticacionCoordenadas.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public IConfiguration Configuration { get; }
+
+        public UsuarioController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Inicio()
+        {
+            return View();
+        }
+
+        public IActionResult Autenticacion()
+        {
+            return View();
+        }
+
+        public IActionResult Bloqueo()
+        {
+            return View();
+        }
+
+        public IActionResult Notificacion()
+        {
+            return View();
+        }
+
+        public IActionResult Peticion()
+        {
+            return View();
+        }
+
+    }
+}
