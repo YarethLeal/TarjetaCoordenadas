9f87fbd2cbee444abd575e34b4e64fa43f3ab518
AracelySC <84749072+AracelySC@users.noreply.github.com>
2021-09-27 23:50:03 -0600

    entities

---
+++ AutenticacionCoordenadas/AutenticacionCoordenadas/Contexts/UsuarioBD.cs
@@ -0,0 +1,18 @@
+﻿using AutenticacionCoordenadas.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutenticacionCoordenadas.Contexts
+{
+    public class UsuarioBD : DbContext
+    {
+        public UsuarioBD(DbContextOptions<UsuarioBD> options) : base(options)
+        {
+
+        }
+        public DbSet<Usuario> tb_Usuario { get; set; }
+    }
+}
---
+++ AutenticacionCoordenadas/AutenticacionCoordenadas/Entities/Oficina.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutenticacionCoordenadas.Entities
+{
+    public class Oficina
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public bool Eliminado { get; set; }
+        public string Usuario { get; set; }
+        public char CodigoOficina { get; set; }
+        public bool Estado { get; set; }
+        public int Institucion { get; set; }
+
+    }
+
+
+}
