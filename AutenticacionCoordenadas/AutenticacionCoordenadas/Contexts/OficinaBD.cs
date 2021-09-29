a714f8d63775eeef71fa5c4c1cf585235b4c4674
AracelySC <84749072+AracelySC@users.noreply.github.com>
2021-09-28 18:41:23 -0600

    view crud oficina

--- AutenticacionCoordenadas/AutenticacionCoordenadas/Contexts/OficinaBD.cs
+++ AutenticacionCoordenadas/AutenticacionCoordenadas/Contexts/OficinaBD.cs
@@ -1,11 +1,18 @@
-﻿using System;
+﻿using AutenticacionCoordenadas.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace AutenticacionCoordenadas.Contexts
 {
-    public class OficinaBD
+    public class OficinaBD : DbContext
     {
+        public OficinaBD(DbContextOptions<OficinaBD> options) : base(options) 
+        {
+        
+        }
+        public DbSet<Oficina> tb_Oficina { get; set; }
     }
 }
