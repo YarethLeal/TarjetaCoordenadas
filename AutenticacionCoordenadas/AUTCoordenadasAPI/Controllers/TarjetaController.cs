97e0c02b1901a643c7eaee43a2da6a13b25e8514
YarethLeal <yarethleal65@gmail.com>
2021-11-17 00:17:57 -0600

    Api-Reportes

--- AutenticacionCoordenadas/AUTCoordenadasAPI/Controllers/TarjetaController.cs
+++ AutenticacionCoordenadas/AUTCoordenadasAPI/Controllers/TarjetaController.cs
@@ -28,5 +28,34 @@ namespace AUTCoordenadasAPI.Controllers
         {
             return new BusinessTarjeta().creacionTarjeta(datosTarjeta);
         }
+        public string obtenerTarjetasEntregadasPorFecha(string fechaI, string fechaF)
+        {
+            return new BusinessTarjeta().obtenerTarjetasEntregadasPorFecha(fechaI, fechaF);
+        }
+
+        public string obtenerTarjetasNegadasPorFecha(string fechaI, string fechaF)
+        {
+            return new BusinessTarjeta().obtenerTarjetasNegadasPorFecha(fechaI, fechaF);
+        }
+
+        public string obtenerTarjetasDesbloqueadasPorFecha(string fechaI, string fechaF)
+        {
+            return new BusinessTarjeta().obtenerTarjetasDesbloqueadasPorFecha(fechaI, fechaF);
+        }
+
+        public string obtenerTarjetasDesbloqueadasPorAdministrador(string administrador)
+        {
+            return new BusinessTarjeta().obtenerTarjetasDesbloqueadasPorAdministrador(administrador);
+        }
+
+        public string obtenerTarjetasNegadasPorAdministrador(string administrador)
+        {
+            return new BusinessTarjeta().obtenerTarjetasNegadasPorAdministrador(administrador);
+        }
+
+        public string obtenerTarjetasEntregadasPorAdministrador(string administrador)
+        {
+            return new BusinessTarjeta().obtenerTarjetasEntregadasPorAdministrador(administrador);
+        }
     }
 }
